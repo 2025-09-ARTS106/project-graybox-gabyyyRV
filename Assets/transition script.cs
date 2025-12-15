67ee4a706142b2238c88097b745f049f70261a5a
gabyyyRV <gbor4671@stu.raritanval.edu>
2025-12-15 14:08:26 -0500

    ui stuff

---
+++ Assets/transition script.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class something : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void StartGame()
+    {
+        SceneManager.LoadScene("graybox project package FINAL");
+    }
+
+
+
+}
+
+// Update is called once per frame
+
+
+
